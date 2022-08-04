e512cb1139c438499f27ebbb347435781b26b929
legendnix <xsport.asd12@gmail.com>
2022-08-04 16:25:32 +0700

    portfolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#22D3EE",
+        secondary: "#64748b",
+        dark: "#0F172A",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
